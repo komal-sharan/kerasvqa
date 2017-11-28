59d6935728ec80b4f1abad2eb18b7c0840720750
Komal Sharan <komalssharan@gmail.com>
2017-11-28 17:46:52 -0500

    Update vis_lstm_model_keras.py

--- vis_lstm_model_keras.py
+++ vis_lstm_model_keras.py
@@ -7,20 +7,19 @@ def build_model(self,options):
 
     embedding_model = Sequential()
     embedding_model.add(Embedding(
-        options['q_vocab_size'] + 1,
+        options['q_vocab_size'],
         options['embedding_size'],
 
-        trainable=False))
+        trainable=True))
 
 
 
     image_model = Sequential()
     image_model.add(Dense(
-        options['fc7_feature_length'],
-        options['embedding_size'],
+        output_dim=512,
         input_dim=4096,
         activation='linear'))
-    image_model.add(Reshape((1, embedding_matrix.shape[1])))
+   
 
     main_model = Sequential()
     main_model.add(Merge(
